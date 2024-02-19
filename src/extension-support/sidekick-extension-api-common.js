13ba27d4e64a759285e3139ef71f5fa30b7df0c8
Maite-Aileen Brandt <maiteaileen@gmail.com>
2024-02-19 07:31:34 +0100

    Revert "add modules"
    This reverts commit cf7a121c6f52696ba4cfe856d640eb8cc08db659.

--- src/extension-support/sidekick-extension-api-common.js
+++ src/extension-support/sidekick-extension-api-common.js
@@ -6,19 +6,17 @@ const Cast = require('../util/cast');
 // const ws281x = require('../util/cast');
 // const ws281x = require('@simontaga/rpi-ws281x-native/lib/ws281x-native');
 // const ws281xControl = require('../util/ws281x-control');
-const sudoJS = require('sudo-js');
-const sudoPrompt = require('sudo-prompt');
+// const sudoJS = require('sudo-js');
 
 
 const Scratch = {
     ArgumentType,
     BlockType,
     TargetType,
-    Cast,
+    Cast
     // ws281x,
     // ws281xControl,
-    sudoJS,
-    sudoPrompt
+    // sudoJS
 };
 
 module.exports = Scratch;
