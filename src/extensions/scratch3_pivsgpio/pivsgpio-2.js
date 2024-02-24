76cc5ba63ba4717b3ea66f2ef392a516bfaeb247
Maite-Aileen Brandt <maiteaileen@gmail.com>
2024-02-24 01:01:00 +0100

    add changes from https://github.com/RaspberryPiFoundation/scratch-desktop

---
+++ src/extensions/scratch3_pivsgpio/pivsgpio-2.js
@@ -0,0 +1,15 @@
+pivsgpio = {
+    name: 'Raspberry Pi Simple Electronics',
+    extensionId: 'pivsgpio',
+    collaborator: 'Raspberry Pi',
+    iconURL: _pivsgpio_pivsgpio_png__WEBPACK_IMPORTED_MODULE_18___default.a,
+    insetIconURL: _pivsgpio_pivsgpio_small_svg__WEBPACK_IMPORTED_MODULE_19___default.a,
+    description: /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_intl__WEBPACK_IMPORTED_MODULE_1__["FormattedMessage"], {
+      defaultMessage: "Simple electronics with Raspberry Pi",
+      id: "gui.extension.pivsgpio.description"
+    }),
+    featured: true
+    
+          
+}
+    
